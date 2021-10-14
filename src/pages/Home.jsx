50282c2d0c351804061b15cec5839a008c7c20b0
Jefferson Uchôa Ponte <jefponte@gmail.com>
2021-10-13 23:08:02 -0300

    Update home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,18 @@
-import { Box, Card, CardActions, CardContent, CardMedia, Container, Grid, Typography } from "@material-ui/core";
+import {
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Container,
+  Grid,
+  Typography,
+} from "@material-ui/core";
 import Button from "@material-ui/core/Button";
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 92, 3, 4, 5, 6, 7, 8, 98, 92, 3, 4, 5, 6, 7, 8, 98, 92, 3, 4, 5, 6, 7, 8, 9];
+const cards = [
+  1, 2, 3, 4, 5, 6, 7, 8, 92, 3, 4, 5, 6, 7, 8, 98, 92, 3, 4, 5, 6, 7, 8, 98,
+  92, 3, 4, 5, 6, 7, 8, 9,
+];
 function Home() {
   return (
     <main>
@@ -12,68 +24,43 @@ function Home() {
           pb: 6,
         }}
       >
-        <Container maxWidth="sm">
-          <Typography
-            component="h1"
-            variant="h2"
-            align="center"
-            color="text.primary"
-            gutterBottom
-          >
-            Album layout
-          </Typography>
-          <Typography
-            variant="h5"
-            align="center"
-            color="text.secondary"
-            paragraph
-          >
-            Something short and leading about the collection below—its contents,
-            the creator, etc. Make it short and sweet, but not too short so
-            folks don&apos;t simply skip over it entirely.
-          </Typography>
-          
+        <Container sx={{ py: 8 }} maxWidth="lx">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xl={1} lg={1}  md={4} sm={6} xs={6} >
+                <Card
+                  sx={{
+                    height: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
+                  <CardMedia
+                    component="img"
+                    sx={{
+                      // 16:9
+                      pt: "56.25%",
+                    }}
+                    image="https://www.themoviedb.org/t/p/w300_and_h450_bestv2/7xOb7DFXHit1G0lsbQ131rLScqg.jpg"
+                    alt="random"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Taxi Driver
+                    </Typography>
+                    <Typography>1975</Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">View</Button>
+                    <Button size="small">Edit</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
         </Container>
       </Box>
-      <Container sx={{ py: 8 }} maxWidth="lx">
-        {/* End hero unit */}
-        <Grid container spacing={4}>
-          {cards.map((card) => (
-            <Grid item key={card} xs={12} sm={4} md={1}>
-              <Card
-                sx={{
-                  height: "100%",
-                  display: "flex",
-                  flexDirection: "column",
-                }}
-              >
-                <CardMedia
-                  component="img"
-                  sx={{
-                    // 16:9
-                    pt: "56.25%",
-                  }}
-                  image="https://www.themoviedb.org/t/p/w300_and_h450_bestv2/7xOb7DFXHit1G0lsbQ131rLScqg.jpg"
-                  alt="random"
-                />
-                <CardContent sx={{ flexGrow: 1 }}>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    Heading
-                  </Typography>
-                  <Typography>
-                    This is a media card. You can use this section to describe
-                    the content.
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button size="small">View</Button>
-                  <Button size="small">Edit</Button>
-                </CardActions>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
-      </Container>
     </main>
   );
 }
