326fd8fa7a583fe7dd69bfb953385c61267d19f8
Jefferson Uchôa Ponte <jefponte@gmail.com>
2021-10-27 00:07:40 -0300

    Fixie bug of no providers.

--- src/components/WatchProviders.jsx
+++ src/components/WatchProviders.jsx
@@ -1,79 +1,109 @@
 import React, { useState } from "react";
 import { Typography } from "@material-ui/core";
 import { FormattedMessage } from "react-intl";
-import { Autocomplete, TextField } from "@mui/material";
-
-function ListProviders(props) {
-  const { idType, providers } = props;
-  return <React.Fragment></React.Fragment>;
-}
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
 
 export default function WatchProviders(props) {
   const { providers } = props;
   let countries = [];
   Object.keys(providers.results).forEach(function (item) {
     let regionNames = new Intl.DisplayNames([item], { type: "region" });
-
-    countries.push({ name: regionNames.of(item), code: item });
+    countries.push({ label: regionNames.of(item), code: item });
   });
 
-  const [country, setCountry] = useState(countries[6]);
+  const [country, setCountry] = useState(countries[0]);
   const types = ["flatrate", "rent", "buy"];
-
-  const handleChangeCountry = (event, values) => {
-    if(values=== null || values === ""){
-      return;
-    }
-    setCountry(values);
-  };
 
   return (
     <>
+      {console.log(countries)}
+
       <Autocomplete
-        id="country"
-        name="country"
+        id="country-select-demo"
+        sx={{ width: 300 }}
         options={countries}
-        getOptionLabel={(option) => option.name}
-        onChange={handleChangeCountry}
+        autoHighlight
         value={country}
-        fullWidth
+        onChange={(event, newValue) => {
+          if(newValue !== null && newValue !== ""){
+            setCountry(newValue);
+          }
+        }}
+        getOptionLabel={(option) => option.label}
+        renderOption={(props, option) => (
+          <Box
+            component="li"
+            sx={{ "& > img": { mr: 2, flexShrink: 0 } }}
+            {...props}
+          >
+            <img
+              loading="lazy"
+              width="20"
+              src={`https://flagcdn.com/w20/${option.code.toLowerCase()}.png`}
+              srcSet={`https://flagcdn.com/w40/${option.code.toLowerCase()}.png 2x`}
+              alt=""
+            />
+            {option.label} ({option.code}) 
+          </Box>
+        )}
         renderInput={(params) => (
           <TextField
-            required
             {...params}
-            label="Country"
-            variant="outlined"
-            margin="normal"
+            label="Choose a country"
+            inputProps={{
+              ...params.inputProps,
+              autoComplete: "new-password", // disable autocomplete and autofill
+            }}
           />
         )}
       />
+      {Object.keys(providers.results[country.code]).map((elemento, index) => {
+        if (types.indexOf(elemento) !== -1) {
+          return (
+            <React.Fragment key={index}>
+              <Typography variant="subtitle1" component="h3">
+                <FormattedMessage
+                  id={`movieSelect.${elemento}`}
+                  description={elemento}
+                  defaultMessage={elemento}
+                />
+              </Typography>
+              {providers.results[country.code][elemento].map((item, index) => {
+                return (
+                  <img
+                    src={`https://image.tmdb.org/t/p/original/${item.logo_path}`}
+                    height={50}
+                    key={item.provider_id}
+                    alt={item.provider_name}
+                  />
+                );
+              })}
+            </React.Fragment>
+          );
+        } else {
+          return <React.Fragment key={index}></React.Fragment>;
+        }
+      })}
 
+      {/*
+       
+      
+
+          
+
+
+     
+      
       <Typography variant="subtitle1" component="p">
         <FormattedMessage
-          id='movieSelect.flatrate'
-          description="flatrate"
-          defaultMessage="flatrate"
-        />
-      </Typography>
-            
-      {providers.results[country.code].flatrate.map((item, index) => {
-        return (
-          <img
-            src={`https://image.tmdb.org/t/p/original/${item.logo_path}`}
-            height={50}
-            key={item.provider_id}
-            alt={item.provider_name}
-          />
-        );
-      })}
-  <Typography variant="subtitle1" component="p">
-        <FormattedMessage
-          id='movieSelect.rent'
+          id="movieSelect.rent"
           description="rent"
           defaultMessage="rent"
         />
       </Typography>
-            
+
       {providers.results[country.code].rent.map((item, index) => {
         return (
           <img
@@ -84,14 +114,14 @@ export default function WatchProviders(props) {
           />
         );
       })}
-  <Typography variant="subtitle1" component="p">
+      <Typography variant="subtitle1" component="p">
         <FormattedMessage
-          id='movieSelect.buy'
+          id="movieSelect.buy"
           description="buy"
           defaultMessage="buy"
         />
       </Typography>
-            
+
       {providers.results[country.code].buy.map((item, index) => {
         return (
           <img
@@ -101,8 +131,7 @@ export default function WatchProviders(props) {
             alt={item.provider_name}
           />
         );
-      })}
-
+      })} */}
     </>
   );
 }
